3ac7cbd15e0b8988e23a85e4cbeff52cd876f3fd
lucas ma <robbincen@163.com>
2019-11-14 22:17:36 +0800

    rest api

--- foodie-api/src/main/java/com/imooc/Application.java
+++ foodie-api/src/main/java/com/imooc/Application.java
@@ -2,12 +2,15 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author mcg
  * @create 2019-11-14-17:37
  */
 @SpringBootApplication
+// 扫描 mybatis 通用 mapper 所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ foodie-service/src/main/java/com/imooc/service/StuService.java
@@ -0,0 +1,20 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Stu;
+
+/**
+ * @Author mcg
+ * @Date 2019/11/14 22:02
+ **/
+
+public interface StuService {
+
+    public Stu getStuInfo(int id);
+
+    public void saveStu();
+
+    public void updateStu(int id);
+
+    public void deleteStu(int id);
+
+}
