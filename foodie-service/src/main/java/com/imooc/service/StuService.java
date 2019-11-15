3411d2e1ef71746cd74bfb61937775278730e99d
chuckma <robbincen@163.com>
2019-11-15 10:15:57 +0800

    事务相关

--- foodie-service/src/main/java/com/imooc/service/StuService.java
+++ foodie-service/src/main/java/com/imooc/service/StuService.java
@@ -17,4 +17,6 @@ public interface StuService {
 
     public void deleteStu(int id);
 
+    public void saveParent();
+    public void saveChildren();
 }
---
+++ foodie-service/src/main/java/com/imooc/service/TestTransService.java
@@ -0,0 +1,9 @@
+package com.imooc.service;
+
+/**
+ * @author mcg
+ * @create 2019-11-15-9:08
+ */
+public interface TestTransService {
+    public void testPropagationTrans();
+}
