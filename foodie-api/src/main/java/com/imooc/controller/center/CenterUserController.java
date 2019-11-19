741f75f9e870216dd4bbf75e7e113315471ef4cd
lucas ma <robbincen@163.com>
2019-11-19 20:26:38 +0800

    验证用户信息

--- foodie-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -10,10 +10,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author mcg
@@ -27,20 +33,68 @@ public class CenterUserController {
     @Autowired
     private CenterUserService centerUserService;
 
-    @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
+   /* @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
     @PostMapping("/update")
     public JSONResult update(@ApiParam(name = "userId", value = "用户Id", required = true)
-                             @RequestParam String userId,
-                             @RequestBody CenterUserBO centerUserBO, HttpServletRequest request,
+                             @RequestParam("userId") String userId,
+                             @RequestBody @Valid CenterUserBO centerUserBO,
+                             BindingResult result,
+                             HttpServletRequest request,
                              HttpServletResponse response) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return JSONResult.errorMap(errorMap);
+        }
 
         Users users = centerUserService.updateUserInfo(userId, centerUserBO);
         Users users1 = setNullProperty(users);
         CookieUtils.setCookie(request, response, "users1", JsonUtils.objectToJson(users1), true);
         // TODO 后续整合 redis 增加 token 分布式会话
         return JSONResult.ok();
+    }*/
+
+    @ApiOperation(value = "修改用户信息", notes = "修改用户信息", httpMethod = "POST")
+    @PostMapping("update")
+    public JSONResult update(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
+            HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println(centerUserBO);
+
+        // 判断BindingResult是否保存错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return JSONResult.errorMap(errorMap);
+        }
+
+        Users userResult = centerUserService.updateUserInfo(userId, centerUserBO);
+
+        userResult = setNullProperty(userResult);
+        CookieUtils.setCookie(request, response, "user",
+                JsonUtils.objectToJson(userResult), true);
+
+        // TODO 后续要改，增加令牌token，会整合进redis，分布式会话
+
+        return JSONResult.ok();
     }
 
+
+    private Map<String, String> getErrors(BindingResult result) {
+        HashMap<String , String > map = new HashMap<>();
+        List<FieldError> errorList = result.getFieldErrors();
+        for (FieldError fieldError : errorList) {
+            // 发生验证错误所对应的某个属性
+            String errorField = fieldError.getField();
+            // 验证错误的信息
+            String errorMsg = fieldError.getDefaultMessage();
+            map.put(errorField, errorMsg);
+        }
+        return map;
+    }
 
     private Users setNullProperty(Users userResult) {
         userResult.setPassword(null);
