388cf900aebe093cb877bc70cb4576022970f974
lucas ma <robbincen@163.com>
2019-11-24 17:46:42 +0800

    上线环境配置

--- foodie-api/src/main/java/com/imooc/config/CorsConfig.java
+++ foodie-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -27,6 +27,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("http://192.168.5.201:8080");
 
         // 设置是否发送 cookie 信息
         config.setAllowCredentials(true);
--- foodie-api/src/main/java/com/imooc/controller/BaseController.java
+++ foodie-api/src/main/java/com/imooc/controller/BaseController.java
@@ -29,7 +29,8 @@ public class BaseController {
     // 微信支付成功->支付中心->foodie 平台
     //                    |-> 回调通知的url
     // String payReturnUrl = "http://localhost:8088/orders/notifyMerchantOrderPaid";
-    String payReturnUrl = "192.168.5.201:8088/foodie-api/orders/notifyMerchantOrderPaid";
+//    String payReturnUrl = "192.168.5.201:8088/foodie-api/orders/notifyMerchantOrderPaid";
+    String payReturnUrl = "http://a9858840.ngrok.io/foodie-api/orders/notifyMerchantOrderPaid";
 
     // 支付中心调用地址
     String paymentUrl = "http://payment.t.mukewang.com/foodie-payment/payment/createMerchantOrder";
